052643b76faa8ddb68dc806a6688990b3cfe9697
Jaycode1996 <57858610+Jaycode1996@users.noreply.github.com>
2020-06-17 15:41:37 +0800

    servo.ts

--- main.ts
+++ main.ts
@@ -13,12 +13,10 @@ namespace ServoCar {
     export class Servo {
         private _minAngle: number;
         private _maxAngle: number;
-        private _stopOnNeutral: boolean;
 
         constructor() {
             this._minAngle = 0;
             this._maxAngle = 180;
-            this._stopOnNeutral = false;
         }
 
         private clampDegrees(degrees: number): number {
@@ -48,23 +46,6 @@ namespace ServoCar {
         }
 
         /**
-         * Stop sending commands to the servo so that its rotation will stop at the current position.
-         */
-        // On a normal servo this will stop the servo where it is, rather than return it to neutral position.
-        // It will also not provide any holding force.
-        //% weight=10 help=servos/stop
-        //% blockId=servoservostop block="stop %servo"
-        //% servo.fieldEditor="gridpicker"
-        //% servo.fieldOptions.width=220
-        //% servo.fieldOptions.columns=2
-        //% parts=microservo trackArgs=0
-        //% group="Servo"
-        stop() {
-            this.internalStop();
-        }
-        protected internalStop() { }
-
-        /**
          * Gets the minimum angle for the servo
          */
         public get minAngle() {
@@ -92,76 +73,21 @@ namespace ServoCar {
             this._pin.servoWrite(angle);
         }
 
-        protected internalSetPulse(micros: number): void {
-            this._pin.servoSetPulse(micros);
-        }
-
-        protected internalStop() {
-            this._pin.digitalWrite(false);
-        }
-
         InternalSetAngle(angle: number): void {
             this._pin.servoWrite(angle);
         }
     }
 
-    let leftWheel = new PinServo(pins.P2);
-    let rightWheel = new PinServo(pins.P1);
-    let shovel = new PinServo(pins.P0);
-
-    /**
-     * Set the car speed
-     * angle can control speed
-     */
-    //% block="Car %directe speed: %Speed"
-    //% Speed.min=0 Speed.max=100
-    //% group="Car"
-    export function Run(directe: DIR, Speed: number) {
-        let CW = Math.map(Speed, 0, 100, 90, 180);
-        let CCW = Math.map(Speed, 0, 100, 90, 0);
-        if (directe == 0) {
-            leftWheel.InternalSetAngle(CW);
-            rightWheel.InternalSetAngle(CCW);
-        }
-        if (directe == 1) {
-            leftWheel.InternalSetAngle(CCW);
-            rightWheel.InternalSetAngle(CW);
-        }
-        if (directe == 2) {
-            leftWheel.InternalSetAngle(CW);
-            rightWheel.InternalSetAngle(CW);
-        }
-        if (directe == 3) {
-            leftWheel.InternalSetAngle(CCW);
-            rightWheel.InternalSetAngle(CCW);
-        }
-    }
-    /**
-     * Car stop
-     */
-    //% block="Car Stop"
-    //% group="Car"
-    export function Stop() {
-        leftWheel.InternalSetAngle(90);
-        rightWheel.InternalSetAngle(90);
-    }
-
-    /**
-     * car's shovel
-     */
-    //% block="Angle of Car's shovel: %angle °"
-    //% angle.min=0 angle.max=90
-    //% group="Car"
-    export function Shovel(angle: number) {
-        shovel.InternalSetAngle(angle);
-    }
+    //% block="servo P0" fixedInstance whenUsed
+    export const P0 = new PinServo(pins.P0);
+    //% block="servo P1" fixedInstance whenUsed
+    export const P1 = new PinServo(pins.P1);
+    //% block="servo P2" fixedInstance whenUsed
+    export const P2 = new PinServo(pins.P2);
+    //% block="servo P3" fixedInstance whenUsed
+    export const P3 = new PinServo(pins.P3);
+    //% block="servo P4" fixedInstance whenUsed
+    export const P4 = new PinServo(pins.P4);
+    //% block="servo P10" fixedInstance whenUsed
+    export const P10 = new PinServo(pins.P10);
 }
-
-enum DIR {
-    runForward = 0,
-    runBack = 1,
-    turnLeft = 3,
-    turnRight = 2
-}
-
-
