497ed165f123ed6712b9661ec2142e7a8753ba09
Jaycode1996 <57858610+Jaycode1996@users.noreply.github.com>
2020-06-17 15:47:52 +0800

    pin.ts

--- targetOverrides.ts
+++ targetOverrides.ts
@@ -5,4 +5,10 @@ namespace servos {
     export const P1 = new ServoCar.PinServo(pins.P1);
     //% block="servo P2" fixedInstance whenUsed
     export const P2 = new ServoCar.PinServo(pins.P2);
+    //% block="servo P3" fixedInstance whenUsed
+    export const P3 = new ServoCar.PinServo(pins.P3);
+    //% block="servo P4" fixedInstance whenUsed
+    export const P4 = new ServoCar.PinServo(pins.P4);
+    //% block="servo P10" fixedInstance whenUsed
+    export const P10 = new ServoCar.PinServo(pins.P10);
 }
