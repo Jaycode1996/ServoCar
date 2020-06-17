16db0cc7bec57519d6c33315ea7f5bd98ea30529
Jaycode1996 <57858610+Jaycode1996@users.noreply.github.com>
2020-06-17 15:48:55 +0800

    servo.ts

--- main.ts
+++ main.ts
@@ -77,17 +77,4 @@ namespace ServoCar {
             this._pin.servoWrite(angle);
         }
     }
-
-    //% block="servo P0" fixedInstance whenUsed
-    export const P0 = new PinServo(pins.P0);
-    //% block="servo P1" fixedInstance whenUsed
-    export const P1 = new PinServo(pins.P1);
-    //% block="servo P2" fixedInstance whenUsed
-    export const P2 = new PinServo(pins.P2);
-    //% block="servo P3" fixedInstance whenUsed
-    export const P3 = new PinServo(pins.P3);
-    //% block="servo P4" fixedInstance whenUsed
-    export const P4 = new PinServo(pins.P4);
-    //% block="servo P10" fixedInstance whenUsed
-    export const P10 = new PinServo(pins.P10);
 }
